7c4135b92bdcf343c80ee5196c86efa1ca3ad7dc
mohamedDev69 <mohamedchettah0208@gmail.com>
2023-12-12 21:50:38 +0100

    ajout des meta tags google

--- app.vue
+++ app.vue
@@ -3,3 +3,28 @@
     <NuxtPage />
   </div>
 </template>
+
+
+<script setup>
+import { useHead } from '@vueuse/head';
+
+
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-0PT349TFG7',
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-0PT349TFG7');
+      `,
+      type: 'text/javascript'
+    }
+  ]
+});
+</script>
